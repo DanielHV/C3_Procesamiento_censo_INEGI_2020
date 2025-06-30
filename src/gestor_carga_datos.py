228f0f5df6a071d7d28ce76d715a74b1bfe98673
Daniel Hernandez <daniel278@ciencias.unam.mx>
2025-06-30 15:58:00 -0600

    Modificaciones mayores gestor_carga_datos
    Se cambia el uso de clases por la definicion de un script que realiza solo las acciones necesarias, es decir, crear la tabla en caso de ser necesaria y subir los datos dado el archivo resultante del procesamiento, utiliza los parametros:
    --ruta-datos-procesados: ruta al archivo .csv generado por el programa main_procesador.py
    --ruta-env: ruta del archivo .env para la conexion a la base de datos
    --crear-tabla: no toma ningun valor, solo se usa para indicar si la tabla debe ser creada

--- src/gestor_carga_datos.py
+++ src/gestor_carga_datos.py
@@ -1,60 +1,63 @@
-import psycopg
+import pandas as pd
+import argparse
 import os
 from dotenv import load_dotenv
+import psycopg
+from psycopg import sql
 from io import StringIO
 
-class GestorCargaDatos:
+if __name__ == "__main__":
     
-    def __init__(self, env_path=".env"):
-        self.env_path = env_path
-        with self.__conectar() as conn:
-            info = conn.info
-            print(f"Conexión verificada exitosamente")
+    parser = argparse.ArgumentParser(description="Gestor de carga de datos procesados a base de datos")
+    parser.add_argument("--ruta-datos-procesados", type=str, required=True, help="Ruta de los datos procesados que serán cargados a la base de datos")
+    parser.add_argument("--ruta-env", type=str, default='./.env', help="Ruta al archivo .env")
+    parser.add_argument("--crear-tabla", action='store_true', help="Adicionalmente crea la tabla especificada en el archivo .env")
+    args = parser.parse_args()
+    
+    df = pd.read_csv(args.ruta_datos_procesados)
 
-    def __conectar(self):
-        load_dotenv(self.env_path, override=True)
-        conn = psycopg.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD")
-        )
-        return conn
+    if not os.path.exists(args.ruta_env):
+        raise FileNotFoundError(f"No se encontró el archivo .env en la ruta: {args.ruta_env}")
 
-    def ejecutar_comando(self, comando):
-        try:
-            with self.__conectar() as conn:
-                with conn.cursor() as cursor:
-                    cursor.execute(comando)
-                    conn.commit()
-                    print("Comando ejecutado exitosamente")
-        except Exception as e:
-            print(f"Error al ejecutar el comando: {e}")
- 
-    def ejecutar_consulta(self, consulta):
-        try:
-            with self.__conectar() as conn:
-                with conn.cursor() as cursor:
-                    cursor.execute(consulta)
-                    resultados = cursor.fetchall()
-                    return resultados
-        except Exception as e:
-            print(f"Error al ejecutar la consulta: {e}")
-            return None
+    load_dotenv(args.ruta_env)
+    
+    with psycopg.connect(
+        host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT"),
+        dbname = os.getenv("DB_NAME"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD")
+    ) as conn:
+        
+        with conn.cursor() as cursor:
+            
+            tabla = os.getenv("DB_TABLE")
+            if args.crear_tabla == True:
+                cursor.execute(sql.SQL("""
+                CREATE TABLE IF NOT EXISTS {} (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(255),
+                    code VARCHAR(255),
+                    bin INT,
+                    interval_state VARCHAR(255),
+                    interval_mun VARCHAR(255),
+                    interval_ageb VARCHAR(255),
+                    cells_state TEXT,
+                    cells_mun TEXT,
+                    cells_ageb TEXT
+                );
+                """).format(sql.Identifier(tabla)))
+        
+            # archivo temporal en memoria
+            buffer = StringIO()
+            df.to_csv(buffer, index=False, header=True)
+            buffer.seek(0)  # volver al inicio del archivo
 
-    def cargar_dataframe_a_tabla(self, df, tabla):
-
-        with self.__conectar() as conn:
-            with conn.cursor() as cursor:
-                # archivo temporal en memoria
-                buffer = StringIO()
-                df.to_csv(buffer, index=False, header=True)
-                buffer.seek(0)  # volver al inicio del archivo
-
-                columnas = ", ".join([f'"{col}"' for col in df.columns])
-                with cursor.copy(f"COPY {tabla} ({columnas}) FROM STDIN WITH CSV HEADER") as copy:
-                    copy.write(buffer.getvalue())
-                
-                conn.commit()
-                print(f"Datos insertados exitosamente en la tabla '{tabla}'")
+            with cursor.copy(sql.SQL("COPY {} ({}) FROM STDIN WITH CSV HEADER").format(
+                sql.Identifier(tabla),
+                sql.SQL(", ").join([sql.Identifier(col) for col in df.columns])
+            )) as copy:
+                copy.write(buffer.getvalue())
+            
+            conn.commit()
+            print(f"Datos insertados exitosamente en la tabla '{tabla}'")
